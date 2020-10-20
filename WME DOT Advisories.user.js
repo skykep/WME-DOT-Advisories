d10e4b7269824a2f67eab06ab608bd617996c4df
skykep <mattkeperling@gmail.com>
2020-10-20 12:27:40 -0400

    PA ramp closure bug fix

--- WME DOT Advisories.user.js
+++ WME DOT Advisories.user.js
@@ -2,7 +2,7 @@
 // @name         WME DOT Advisories
 // @namespace    https://greasyfork.org/en/users/668704-phuz
 // @require      https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
-// @version      1.07
+// @version      1.08
 // @description  Overlay DOT Advisories on the WME Map Object
 // @author       phuz
 // @include      /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
@@ -206,6 +206,7 @@ const NotNY = ['Pennsylvania Statewide', 'New Jersey Statewide', 'Connecticut St
             var resultObj = JSON.parse(result.responseText);
             var i=0;
             var icon;
+            var x,y;
             while (i<resultObj.length) {
                 if ((resultObj[i].LaneStatus == "closed") || (resultObj[i].LaneStatus == "ramp closure")) {
                     switch(resultObj[i].EventType) {
@@ -215,7 +216,14 @@ const NotNY = ['Pennsylvania Statewide', 'New Jersey Statewide', 'Connecticut St
                         default:
                             icon = "Incident";
                     }
-                    drawMarkers("PA",resultObj[i].EventID,resultObj[i].Facility,resultObj[i].FromLocLatLong.split(",")[1],resultObj[i].FromLocLatLong.split(",")[0],icon,resultObj[i].Description,resultObj[i].LastUpdate,"");
+                    if (resultObj[i].LaneStatus != "ramp closure") {
+                        x = resultObj[i].FromLocLatLong.split(",")[1];
+                        y = resultObj[i].FromLocLatLong.split(",")[0];
+                    } else {
+                        x = resultObj[i].IncidentLocLatLong.split(",")[1];
+                        y = resultObj[i].IncidentLocLatLong.split(",")[0];
+                    }
+                    drawMarkers("PA",resultObj[i].EventID,resultObj[i].Facility,x,y,icon,resultObj[i].Description,resultObj[i].LastUpdate,"");
                 }
                 i++;
             }
@@ -343,47 +351,46 @@ const NotNY = ['Pennsylvania Statewide', 'New Jersey Statewide', 'Connecticut St
     }
 
     // Make the DIV element draggable:
+    function dragElement(elmnt) {
+        var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+        if (document.getElementById("mydivheader")) {
+            // if present, the header is where you move the DIV from:
+            document.getElementById("mydivheader").onmousedown = dragMouseDown;
+        } else {
+            // otherwise, move the DIV from anywhere inside the DIV:
+            elmnt.onmousedown = dragMouseDown;
+        }
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById("mydivheader")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById("mydivheader").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+    }
 
     function initializeSettings()
     {
@@ -499,7 +506,7 @@ function dragElement(elmnt) {
         });
     }
     //--- CSS styles make it work...
-    GM_addStyle ("                                      \
+    GM_addStyle ("                                     \
 hr.myhrline{\
 margin: 5px;\
 }\
@@ -520,7 +527,7 @@ background-color: #2f2f2f;\
 cursor:                 pointer;                        \
 margin:                 1em 1em 0;                      \
 border:                 1px outset buttonface;          \
-}\                                                       \
+}\
 .modalclose {\
 background: lightgray;\
 color: #FFFFFF;\
